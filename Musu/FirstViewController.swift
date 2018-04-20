93c615532e7a1433ea04cfb0554e1f72da2b1142
Richard L Zarth III <rlziii@icloud.com>
2018-04-19 20:47:17 -0400

    Added jsonTest to send a JSON to Musū remote API and get a response.

--- Musu/FirstViewController.swift
+++ Musu/FirstViewController.swift
@@ -6,6 +6,13 @@
 //  Copyright © 2018 RLZIII. All rights reserved.
 //
 
+/*
+ * TODO LIST
+ *
+ * Set NSAllowsArbitraryLoads to NO (Info.plist)
+ *
+ */
+
 import UIKit
 
 class FirstViewController: UIViewController {
@@ -13,13 +20,54 @@ class FirstViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        jsonTest()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func jsonTest() {
+        let parameters = ["function": "loginAttempt", "username": "test", "password": "test"] as Dictionary<String, String>
+        
+        let url = URL(string: "http://www.musuapp.com/API/API.php")!
+        
+        let session = URLSession.shared
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted)
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        let task = session.dataTask(with: request as URLRequest, completionHandler: {data, response, error in
+            guard error == nil else {
+                return
+            }
+            
+            guard let data = data else {
+                return
+            }
+            
+            do {
+                if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
+                    print(json)
+                }
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        })
+        
+        task.resume()
+    }
 
 }
 
